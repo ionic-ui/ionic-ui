cbfe795190984ff40d019364cd84e4f5c550ce81
Mickael Marquez <marquez.mickael@gmail.com>
2017-04-26 14:52:36 +0200

    first commit

---
+++ src/pages/blank/blank.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@Component({
+  selector: 'page-blank',
+  templateUrl: 'blank.html',
+})
+export class Blank {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+}
